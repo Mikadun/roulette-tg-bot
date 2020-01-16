8fedcdd04a284f448d1745447af4cd6862340e3b
JIeHb <gribak98@list.ru>
2020-01-16 19:53:41 +1000

    small changes in utils

--- modules/utils.py
+++ modules/utils.py
@@ -1,11 +1,8 @@
 import json
 from modules.db_manager import unauth_users, auth_users
 
-def is_fefu_email(message):
-    email = message.text.strip()
-    if unauth_users.check_user_id(message.from_user.id):
-        return False
-
+def is_fefu_email(email):
+    email = email.strip()
     if email.count('@dvfu.ru') + email.count('@students.dvfu.ru') == 1:
         email = email.split('@')[0]
         print(email)
@@ -16,35 +13,21 @@ def is_fefu_email(message):
             return True
     return False
 
-def is_code(message):
-    if not em_conf.get_state(message.from_user.id) == STATES['expect_code']:
-        return False
-
-    text = message.text
-    return text.isnumeric() and 1000 <= int(text) <= 9999
+def is_code(code):
+    return code.isnumeric() and 1000 <= int(code) <= 9999
 
 def is_group(message):
-    if not em_conf.get_state(message.from_user.id) == STATES['expect_group']:
-        return False
-    
     return True
 
 def is_full_name(name):
-        data = name.split()
-        if len(data) != 3:
-                return False
-        for i in data:
-                if not i.isalpha():
-                        return False
-        return data
-    
-def get_info(user_id):
-    result = users.get_info(user_id)
-    if result == -1:
-        return "You're not registered"
-    else:
-        id, tg_id, name, group, score, email = result
-        return ' '.join((str(i).strip() for i in result))
+    data = name.split()
+    if len(data) != 3:
+        return False
+
+    for i in data:
+        if not i.isalpha():
+            return False
+    return data
 
 if __name__ == '__main__':
     print(get_token())
