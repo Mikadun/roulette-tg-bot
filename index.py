3d2e9dca67cfce05c85a058595a87850de155914
Joker1379 <44800393+Joker1379@users.noreply.github.com>
2020-01-16 18:20:49 +1000

    Update authentification feature

--- index.py
+++ index.py
@@ -5,7 +5,28 @@ bot = telebot.TeleBot(token = getToken())
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Hello')
+
+@bot.message_handler(func = is_fefu_email)
+def got_email(message):
+    if send_code(message.text, message.from_user.id) == -1:
+        bot.send_message(message.chat.id, 'This tg account or email was already registered')
+    else:
+        bot.send_message(message.chat.id, 'Send code to {email}. Write your auth code'.format(email = message.text))
+
+@bot.message_handler(func = is_code)
+def got_code(message):
+    code = int(message.text)
+    if check_code(message.from_user.id, code):
+        bot.send_message(message.chat.id, 'Write you group')
+    else:
+        bot.send_message(message.chat.id, 'Wrong code, try again')
+
+@bot.message_handler(func = is_group)
+def got_group(message):
+    group = message.text
+    register(message.from_user.id, group)
+    bot.send_message(message.chat.id, 'Succesfully registered')
 
 print('Bot is running')
 bot.polling()
--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,8 @@
 import json
+from modules.bot_db import email_confirmation as em_conf, users
+from transliterate import translit
 
-def getToken():
+def get_token():
     file_path = 'telegram-token.json'
     try:
         return json.load(open(file_path))['token']
@@ -10,7 +12,55 @@ def getToken():
         for c in [' ', '\n', '"', "'"]:
             token = token.strip(c)
         json.dump({'token': token}, open(file_path, 'w'))
-        return getToken()
+        return get_token()
+
+def is_fefu_email(message):
+    email = message.text.strip()
+    if users.check_user_id(message.from_user.id):
+        return False
+
+    if email.count('@dvfu.ru') + email.count('@students.dvfu.ru') == 1:
+        email = email.split('@')[0]
+        print(email)
+        if email.count('.') + email.count('_') == 1:
+            for i in range(len(email)):
+                if (ord(email[i])<97 or ord(email[i])>122) and email[i]!='.' and email[i]!='_':
+                    return False
+            return True
+    return False
+
+STATES = {'expect_code': 0, 'expect_group': 1}
+def is_code(message):
+    if not em_conf.get_state(message.from_user.id) == STATES['expect_code']:
+        return False
+
+    text = message.text
+    return text.isnumeric() and 1000 <= int(text) <= 9999
+
+def is_group(message):
+    if not em_conf.get_state(message.from_user.id) == STATES['expect_group']:
+        return False
+    
+    return True
+
+def get_name(email):
+    left_part = email.split('@')[0]
+    dividers = ['.', '_']
+
+    for divider in dividers:
+        if divider in left_part:
+            name = ' '.join(left_part.split(divider))
+            return translit(name, language_code='ru', reversed=True)
+
+    return translit(left_part, language_code='ru', reversed=True)
+
+def get_info(user_id):
+    result = users.get_info(user_id)
+    if result == -1:
+        return "You're not registered"
+    else:
+        id, tg_id, name, group, score, email = result
+        return ' '.join((str(i).strip() for i in result))
 
 if __name__ == '__main__':
-    print(getToken())
+    print(get_token())
