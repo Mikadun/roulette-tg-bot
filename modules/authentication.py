f5a64059120b27092204f3837022f16922d54efd
JIeHb <gribak98@list.ru>
2020-01-18 21:09:13 +1000

    Auth progress

--- modules/authentication.py
+++ modules/authentication.py
@@ -5,9 +5,7 @@ from random import randint
 def send_code(user_id, email):
     MIN, MAX = 1000, 9999
     code = randint(MIN, MAX)
-    unauth_users.update_email(user_id, email)
-    unauth_users.update_code(user_id, code)
-    unauth_users.next_state(user_id)
+    unauth_users.update_email(user_id, email, code)
     verification_mail(email, code)
 
 def check_code(user_id, code):
@@ -19,17 +17,18 @@ def check_code(user_id, code):
         return False
 
 def start_registration(user_id):
-    if unauth_users.add(user_id) == -1:
+    if auth_users.check_user_id(user_id) or unauth_users.add(user_id) == -1:
         return False
-    unauth_users.next_state(user_id)
     return True
 
 def add_full_name(user_id, full_name):
     unauth_users.update_name(user_id, full_name[1], full_name[2], full_name[0])
-    unauth_users.next_state(user_id)
 
 def register(user_id, group):
-    email = unauth_users.get_email(user_id)
+    info = unauth_users.get_info(user_id)
+    auth_users.add(*info[1:5], group, info[6])
     #auth_users.add(get_name(email), email, group, user_id)
     unauth_users.delete(user_id)
 
+unauth_users.clear()
+auth_users.clear()
