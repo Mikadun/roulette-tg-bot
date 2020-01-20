37ebcecce6a92165644dc161b34a4a425d021ee2
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 21:36:04 +1000

    Update db_roulettes.py

--- modules/db_roulettes.py
+++ modules/db_roulettes.py
@@ -18,17 +18,19 @@ class Classic_roulette():
 	def add(self, ref_id, user_id, bet, value):
 		try:
 			self.cur.execute('''INSERT INTO "Russian_roulette" ("Reference_ID", "Tg_ID", "Bet", "Value") VALUES (%s, %s, %s, %s)''', (ref_id, user_id, bet, value))
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else:
+			self.conn.commit()
+			return True
 
 	def delete(self, ref_id, user_id):
 		try:
 			self.cur.execute('''DELETE FROM "Classic_roulette" WHERE ("Reference_ID = %s" AND "Tg_ID = %s")''', (ref_id, user_id))
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else:
+			self.conn.commit()
+			return True
 
 classic_roulette = Classic_roulette()
