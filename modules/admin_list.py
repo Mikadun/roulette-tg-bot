1da185c6bf4156780ab7ace6cccd6aae3aa72685
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 21:37:14 +1000

    Update admin_list.py

--- modules/admin_list.py
+++ modules/admin_list.py
@@ -16,10 +16,11 @@ class Admin_list():
 	def add(self, user_id):
 		try:
 			self.cur.execute('''INSERT INTO "Admin_list" ("Tg_ID") VALUES (%s)''', (user_id,))
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else:
+			self.conn.commit()
+			return True
 
 	def check(self, user_id):
 		try:
