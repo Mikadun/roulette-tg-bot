d8b8cce5919d48eeaa5eb3d82406d58f3173ada1
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 18:29:29 +1000

    Create admin_list.py

---
+++ modules/admin_list.py
@@ -0,0 +1,21 @@
+import os
+import urllib.parse as up
+import psycopg2
+
+class Admin_list():
+	def __init__(self):
+		self.conn = psycopg2.connect(dbname=os.getenv('DB_NAME'), user=os.getenv('DB_NAME'), 
+									password=os.getenv('DB_PASSWORD'), 
+									host=os.getenv('DB_HOST'))
+		self.cur = self.conn.cursor()
+
+	def __del__(self):
+		self.cur.close()
+		self.conn.close()
+
+	check(self, user_id):
+		try:
+			self.cur.execute('''SELECT * FROM "Admin_list" WHERE ("Tg_ID" = %s)''', (user_id,))
+			return not(self.cur.fetchall() == [])
+		except:
+			return -1
