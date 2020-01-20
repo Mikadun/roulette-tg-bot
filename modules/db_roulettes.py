0899143d0f0171f1fc49ac3b0fe220f7162774ab
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 21:41:36 +1000

    Update db_roulettes.py

--- modules/db_roulettes.py
+++ modules/db_roulettes.py
@@ -17,12 +17,22 @@ class Russian_roulette():
 
 	def add(self, ref_id, magazine, bullet, misfire):
 		try:
-			self.cur.execute('''INSERT INTO "Russian_roulette" ("Reference_ID", "Magazine", "Shoots", "Bullet", "Misfire") VALUES (%s, %s, %s, %s, %s)''', (ref_id, magazine, 0, bullet, misfire))
+			if (self.check_ref_id(ref_id)):
+				self.cur.execute('''INSERT INTO "Russian_roulette" ("Reference_ID", "Magazine", "Shoots", "Bullet", "Misfire") VALUES (%s, %s, %s, %s, %s)''', (ref_id, magazine, 0, bullet, misfire))
+			else:
+				return False
 		except:
 			return False
 		else: 
 			self.conn.commit()
 			return True
+	
+	def check_ref_id(self, ref_id):
+		try:
+			self.cur.execute('''SELECT * FROM "Russian_roulette" WHERE ("Reference_ID" = %s)''', (ref_id,))
+			return (self.cur.fetchall() == [])
+		except:
+			return -1
 
 	def shoot(self, ref_id):
 		try:
