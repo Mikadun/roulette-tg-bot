4fa9f34efde31e18157aa0304fcbd753d99e8748
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 21:35:10 +1000

    Update db_roulettes.py

--- modules/db_roulettes.py
+++ modules/db_roulettes.py
@@ -18,45 +18,43 @@ class Russian_roulette():
 	def add(self, ref_id, magazine, bullet, misfire):
 		try:
 			self.cur.execute('''INSERT INTO "Russian_roulette" ("Reference_ID", "Magazine", "Shoots", "Bullet", "Misfire") VALUES (%s, %s, %s, %s, %s)''', (ref_id, magazine, 0, bullet, misfire))
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else: 
+			self.conn.commit()
+			return True
 
 	def shoot(self, ref_id):
 		try:
 			self.cur.execute('''SELECT * FROM "Russian_roulette" WHERE ("Reference_ID" = %s)''', (ref_id, ))
 			temp = self.cur.fetchall()[0]
-		except Exception as e:
-			print(1, e)
-			return False
 
-		try:
 			if (temp[3]+1 == temp[4]):
 				self.cur.execute('''DELETE FROM "Russian_roulette" WHERE ("Reference_ID" = %s)''', (ref_id, ))
 			else:
 				self.cur.execute('''UPDATE "Russian_roulette" SET "Shoots" = %s where "Reference_ID" = %s''', (temp[3]+1, ref_id))
-
+		except:
+			return False	
+		else:
 			self.conn.commit()
 			return [(temp[3]+1 == temp[4]), temp[5]]
-		except Exception as e:
-			print(2, e)
-			return False	
 
 	def delete(self, ref_id):
 		try:
 			self.cur.execute('''DELETE FROM "Russian_roulette" WHERE ("Reference_ID" = %s)''', (ref_id, ))
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else:
+			self.conn.commit()
+			return True
 
 	def clear(self):
 		try:
 			self.cur.execute('''DELETE FROM "Russian_roulette"''')
-			self.conn.commit()
-			return True
 		except:
 			return False
+		else:
+			self.conn.commit()
+			return True
 
 russian_roulette = Russian_roulette()
