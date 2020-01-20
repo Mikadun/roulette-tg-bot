f28b147c8306c8da08eae1b77af32063555a189d
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-01-20 21:09:05 +1000

    Update db_roulettes.py

--- modules/db_roulettes.py
+++ modules/db_roulettes.py
@@ -35,7 +35,8 @@ class Russian_roulette():
 
 			self.conn.commit()
 			return [(temp[3]+1 == temp[4]), temp[5]]
-		except:
+		except Exception as e:
+			print(e)
 			return False	
 
 	def delete(self, ref_id):
