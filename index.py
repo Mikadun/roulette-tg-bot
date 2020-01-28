72c6c3e679021c4bd1902e3ec5b0bf973efbc3af
JIeHb <gribak98@list.ru>
2020-01-28 16:40:06 +1000

    Added webhook

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 import telebot
 import os
 
-bot = telebot.TeleBot(token = os.getenv('TOKEN'))
+APP_NAME = os.getenv('APP_NAME')
+TOKEN = os.getenv('TOKEN')
+
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
 	bot.send_message(message.chat.id, 'Hello')
 
 print('Bot is running')
+
+bot.set_webhook("https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
 bot.polling()
