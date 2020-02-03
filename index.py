3b2cc2808692a48a0732d3b458c47782b8b27437
Joker1379 <44800393+Joker1379@users.noreply.github.com>
2020-02-03 17:05:17 +1000

    Add keyboard test

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import telebot
 from modules.utils import getToken
 from modules.db_roulettes import Classic_roulette
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 bot = telebot.TeleBot(token = getToken())
 
@@ -15,6 +16,25 @@ def classic_roulette_bet(message):
         Classic_roulette().add(message.chat.id, message.from_user.id, data[0], data[1])
     except:
         bot.send_message(message.chat.id, 'Wrong command/')
+
+###
+def gen_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Yes", callback_data="cb_yes"),
+                               InlineKeyboardButton("No", callback_data="cb_no"))
+    return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == "cb_yes":
+        bot.answer_callback_query(call.id, "Answer is Yes")
+    elif call.data == "cb_no":
+        bot.answer_callback_query(call.id, "Answer is No")
+
+@bot.message_handler(commands=['key'])
+def message_handler(message):
+    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
 
 print('Bot is running')
 bot.polling()
---
+++ run.bat
@@ -0,0 +1,5 @@
+SET DB_NAME=twzduxvi
+SET DB_PASSWORD=X5t-ZuJISuLog3Wdc5JuXSApLnEga07O
+SET DB_HOST=rajje.db.elephantsql.com
+SET TOKEN=916124889:AAGAlUJWjkvdAtpq8uT31iIOZPQyrmgGaz0
+python index.py
