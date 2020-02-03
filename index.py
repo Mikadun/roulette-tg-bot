b3d5d6ac5526b82e3eedc1848a23a1dced45e8b1
Joker1379 <44800393+Joker1379@users.noreply.github.com>
2020-02-03 16:46:11 +1000

    Add classic_roulette_bet

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
 import telebot
 from modules.utils import getToken
+from modules.db_roulettes import Classic_roulette
 
 bot = telebot.TeleBot(token = getToken())
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Hello')
+
+@bot.message_handler(commands=['bet'])
+def classic_roulette_bet(message):
+    try:
+        data = message.text.split()
+        Classic_roulette().add(message.chat.id, message.from_user.id, data[0], data[1])
+    except:
+        bot.send_message(message.chat.id, 'Wrong command/')
 
 print('Bot is running')
 bot.polling()
