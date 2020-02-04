59f9104069fabef7ae753f1c2750a54612740ada
JIeHb <gribak98@list.ru>
2020-02-04 15:43:01 +1000

    Launch for dev and prod

--- index.py
+++ index.py
@@ -5,11 +5,9 @@ from modules import utils, authentication
 from modules.states import states
 from modules.db_manager import unauth_users
 
-APP_NAME = os.getenv('HEROKU_APP_NAME')
 TOKEN = os.getenv('TOKEN')
 
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -57,18 +55,27 @@ def got_group(message):
 def any_message(message):
     bot.send_message(message.chat.id, 'Write /help for command list')
 
-print('Bot is running')
-
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-	return "!", 200
-
-@server.route("/")
-def webhook():
-	bot.remove_webhook()
-	bot.set_webhook("https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-	return "!", 200
 
 if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    print('Bot is running')
+
+    if os.getenv('DEV'):
+        bot.remove_webhook()
+        bot.infinity_polling()
+    else:
+        APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+        server = Flask(__name__)
+
+        @server.route('/' + TOKEN, methods=['POST'])
+        def get_message():
+            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+            return "!", 200
+
+        @server.route("/")
+        def webhook():
+            bot.remove_webhook()
+            bot.set_webhook("https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
+            return "!", 200
+
+        server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
