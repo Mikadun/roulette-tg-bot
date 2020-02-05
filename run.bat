d07570919469c22501fdaf2e1a97c55a474e3af9
JIeHb <gribak98@list.ru>
2020-02-05 13:11:17 +1000

    Small changes

--- run.bat
+++ run.bat
@@ -4,4 +4,5 @@ SET DB_HOST=rajje.db.elephantsql.com
 SET TOKEN=916124889:AAGAlUJWjkvdAtpq8uT31iIOZPQyrmgGaz0
 SET EMAIL_HOST=suitable.nickname@gmail.com
 SET EMAIL_PASSWORD=s853458s
+SET DEV=1
 python index.py
