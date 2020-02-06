0f54ad141d6c4bf9b6641ff6dc7053ed4e1ceff3
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-02-07 01:04:11 +1000

    Update roulettes.py

--- modules/roulettes.py
+++ modules/roulettes.py
@@ -1,7 +1,9 @@
 from random import randint, choice
 from secrets import choice
+from index import bot
 
-#def f_roulette(players, good = int(round(len(players)/6)), bad = int(len(players)/6), win = "#fio# win", lose = "#fio# lose"):
+
+def f_roulette(players, good = int(round(len(players)/6)), bad = int(len(players)/6), win = "#fio# win", lose = "#fio# lose"):
 
 def chose_line(text):
     return choice(text)
