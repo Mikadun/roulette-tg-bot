44e7bd748c4438f0fbfa822781241e2dbea4dcf7
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-02-07 01:43:28 +1000

    Update roulettes.py

--- modules/roulettes.py
+++ modules/roulettes.py
@@ -3,7 +3,21 @@ from secrets import choice
 from index import bot
 
 
-def f_roulette(players, good = int(round(len(players)/6)), bad = int(len(players)/6), win = "#fio# win", lose = "#fio# lose"):
+def f_roulette(players):
+    n = len(players)
+
+    win = set()
+    lose = set()
+
+    while len(win) <= round(n/6):
+        win.add(players[randint(0, n)])
+
+    while len(lose) <= int(n/3):
+        r = randint(0, n)
+        if not(players[r] in win):
+            lose.add(players[r])
+
+    return [win, lose]
 
 def chose_line(text):
     return choice(text)
