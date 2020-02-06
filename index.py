b214e7f4f2c894f014e3e44920b35b927c2a22bf
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-02-07 00:05:21 +1000

    Update index.py

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-	bot.send_message(message.chat.id, 'Hello')
+    bot.send_message(message.chat.id, 'Hello')
 
 print('Bot is running')
 
