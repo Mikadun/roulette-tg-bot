34409c43bbcca762b00628d6f55cb1a0816fae37
Vad47z <46963949+Vad47z@users.noreply.github.com>
2020-02-07 02:56:02 +1000

    Update roulettes.py

--- modules/roulettes.py
+++ modules/roulettes.py
@@ -9,15 +9,15 @@ def f_roulette(players):
     win = set()
     lose = set()
 
-    while len(win) <= round(n/6):
-        win.add(players[randint(0, n)])
+    while len(win) < round(n/6):
+        win.add(players[randint(0, n-1)])
 
-    while len(lose) <= int(n/3):
-        r = randint(0, n)
+    while len(lose) < int(n/3):
+        r = randint(0, n-1)
         if not(players[r] in win):
             lose.add(players[r])
 
-    return [win, lose]
+    return [list(win), list(lose)]
 
 def chose_line(text):
     return choice(text)
